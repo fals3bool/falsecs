cb39fce2e02cf53d31b521bd3bd7c065a76b90fc
fals3bool <fabritomas25@gmail.com>
2026-07-09 23:52:26 -0300

    collider & rigidbody components Collider creation helper functions ColliderCube Vector3 implementation

--- src/ecs/component/collider.c
+++ src/ecs/component/collider.c
@@ -1,47 +1,44 @@
 #include <ecs/component.h>
 
 #include <stdlib.h>
+#include <string.h>
 
-Collider ColliderCreate(int vertices, float radius, bool solid) {
-  Collider col = {0};
-  col.vx = (Vector2 *)malloc(sizeof(Vector2) * vertices);
-  col.md = (Vector2 *)malloc(sizeof(Vector2) * vertices);
-  col.vertices = vertices;
-  col.solid = solid;
-
-  float angle = PI * 2 / vertices;
-  for (int i = 0; i < vertices; i++) {
-    col.vx[i] = (Vector2){radius * cosf(angle * i), radius * sinf(angle * i)};
-    col.md[i] = col.vx[i];
-  }
-
-  return col;
+void InitVertices(Collider *c, Vector3 *v, uint8_t len) {
+  c->vertices = len;
+  c->vx = (Vector3 *)malloc(sizeof(Vector3) * len);
+  c->md = (Vector3 *)malloc(sizeof(Vector3) * len);
+  memcpy(c->md, v, sizeof(Vector3) * len);
+  memcpy(c->vx, v, sizeof(Vector3) * len);
 }
 
-Collider ColliderVec(uint8_t vertices, Vector2 *vecs, bool solid) {
-  Collider col = {0};
-  col.vertices = vertices;
-  col.solid = solid;
-  col.vx = (Vector2 *)malloc(sizeof(Vector2) * vertices);
-  col.md = (Vector2 *)malloc(sizeof(Vector2) * vertices);
-
-  for (uint8_t i = 0; i < vertices; i++) {
-    col.vx[i] = vecs[i];
-    col.md[i] = vecs[i];
-  }
-  return col;
+void InitEdges(Collider *c, uint8_t *e, uint8_t len) {
+  c->edges = len / 2;
+  c->edge = (uint8_t *)malloc(len);
+  memcpy(c->edge, e, len);
 }
 
-Collider ColliderRect(Rectangle rect, bool solid) {
-  Vector2 vecs[] = {{rect.x, rect.y},
-                    {rect.x, rect.y + rect.height},
-                    {rect.x + rect.width, rect.y + rect.height},
-                    {rect.x + rect.width, rect.y}};
-  return ColliderVec(4, vecs, solid);
+Collider ColliderCube(float size, bool solid) {
+  Collider c = {0};
+
+  float s = size / 2;
+  Vector3 cube[8] = {{-s, -s, -s}, {-s, -s, s}, {s, -s, -s}, {s, -s, s},
+                     {-s, s, -s},  {-s, s, s},  {s, s, -s},  {s, s, s}};
+  uint8_t edges[24] = {// bottom face
+                       0, 1, 0, 2, 3, 1, 3, 2,
+                       // top face
+                       4, 5, 4, 6, 7, 5, 7, 6,
+                       // vertical edges
+                       0, 4, 1, 5, 2, 6, 3, 7};
+
+  InitVertices(&c, cube, 8);
+  InitEdges(&c, edges, 24);
+  c.solid = solid;
+  return c;
 }
 
 void ColliderDestructor(void *_self) {
   Collider *self = (Collider *)_self;
   free(self->md);
   free(self->vx);
+  free(self->edge);
 }
