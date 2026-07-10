1c0f652a69f569540ce04ea64768a8f6eb75bcfa
fals3bool <fabritomas25@gmail.com>
2026-07-10 00:44:58 -0300

    removed Empty macro

--- src/ecs/component/behaviour.c
+++ src/ecs/component/behaviour.c
@@ -3,7 +3,7 @@
 void AddScript(ECS *ecs, Entity e, Script s, EcsPhase phase) {
   Behaviour *beh = GetComponent(ecs, e, Behaviour);
   if (!beh) {
-    AddComponent(ecs, e, Behaviour, Empty);
+    AddComponent(ecs, e, Behaviour, {0});
     beh = GetComponent(ecs, e, Behaviour);
   }
   beh->scripts[phase] = s;
